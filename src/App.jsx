24270a9518fce83666c43d3fece9fa0b22815361
Breno Henrique <breno.hs@outlook.com>
2024-05-11 11:23:58 -0300

    corrigindo rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Contrast from './Contrast';
-import Cores from './Cores';
 import Header from './Header';
 import './App.css';
 
@@ -12,8 +10,6 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='contraste' element={<Contrast />} />
-        <Route path='cores' element={<Cores />} />
       </Routes>
     </BrowserRouter>
   );
